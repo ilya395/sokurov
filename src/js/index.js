c54bf402aeb0173eb6f24b90735dab37564eac70
ilya395 <ilya395.efimenko@yandex.ru>
2020-11-01 00:03:20 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+console.log('Hello, bro! This project work with Webpack!');
+//
+// import 'normalize.css';
+
+// import 'materialize-css/dist/css/materialize.min.css';
+// import 'materialize-css/dist/js/materialize.min';
+
+import './materialize-src/js/bin/materialize.min'; 
+import '../sass/style.scss';
