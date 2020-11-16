d2ec32d3db0ec278f574c643b3afa2a6cec9c459
Ольга Евграфова <olgaevgrafova@MacBook-Air-Olga.local>
2020-11-16 20:53:43 +0300

    add newspage layout and sliders scripts

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ console.log('Hello, bro! This project work with Webpack!');
 //
 
 // import Swiper JS
-import Swiper from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 // import Swiper styles
 import 'swiper/swiper-bundle.css';
 //
@@ -17,7 +17,9 @@ import '../sass/style.scss';
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    const swiper = new Swiper();
+    Swiper.use([Navigation, Pagination]);
+
+    //const swiper = new Swiper();
 
     //параллакс фона
     function parallax(){
@@ -90,8 +92,111 @@ $( document ).ready(function() {
     //фото в блоке аллея
     $('.alley-interactive .plus').click(function() {
     	let thisPlus = $(this);
-    	$('.alley-interactive .plus__photo').removeClass('active');
-    	thisPlus.find('.plus__photo').addClass('active');
+        if (thisPlus.find('.plus__photo').hasClass('active')) {
+            thisPlus.find('.plus__photo').removeClass('active');
+        } else {
+            $('.alley-interactive .plus__photo').removeClass('active');
+            thisPlus.find('.plus__photo').addClass('active');
+        }
+    });
+
+    //слайдеры
+    $('.main-slider__wrapper .arrow-prev').click(function() {
+        if($(this).hasClass('disabled') == false) {
+            let wrapper = $('.main-slider__wrapper');
+            let count = wrapper.find('.main-slider__slide').length;
+            let actInd = wrapper.find('.main-slider__slide.active').index();
+            if (actInd > 0) {
+                $('.main-slider__wrapper .arrow-next').removeClass('disabled');
+                wrapper.find('.main-slider__slide').fadeOut(300);
+                wrapper.find('.main-slider__slide').removeClass('active');
+                wrapper.find('.pagination-item').removeClass('active');
+                wrapper.find('.main-slider__slide').eq(actInd - 1).fadeIn(300);
+                wrapper.find('.main-slider__slide').eq(actInd - 1).addClass('active');
+                wrapper.find('.pagination-item').eq(actInd - 1).addClass('active');
+                if(actInd == 1) {
+                    $('.main-slider__wrapper .arrow-prev').addClass('disabled');
+                }
+            }
+        }
+    });
+
+    $('.main-slider__wrapper .arrow-next').click(function() {
+        if($(this).hasClass('disabled') == false) {
+            let wrapper = $('.main-slider__wrapper');
+            let count = wrapper.find('.main-slider__slide').length;
+            let actInd = wrapper.find('.main-slider__slide.active').index();
+            if (actInd < count - 1) {
+                $('.main-slider__wrapper .arrow-prev').removeClass('disabled');
+                wrapper.find('.main-slider__slide').fadeOut(300);
+                wrapper.find('.main-slider__slide').removeClass('active');
+                wrapper.find('.pagination-item').removeClass('active');
+                wrapper.find('.main-slider__slide').eq(actInd + 1).fadeIn(300);
+                wrapper.find('.main-slider__slide').eq(actInd + 1).addClass('active');
+                wrapper.find('.pagination-item').eq(actInd + 1).addClass('active');
+                if(actInd == count - 2) {
+                    $('.main-slider__wrapper .arrow-next').addClass('disabled');
+                }
+            }
+        }
+    });
+
+    const alleySwiper = new Swiper('#alley-slider .swiper-container', {
+        navigation: {
+            nextEl: '#alley-slider .swiper-button-next',
+            prevEl: '#alley-slider .swiper-button-prev',
+        },
+        pagination: {
+            el: '#alley-slider .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        }
+    });
+
+    const constructionSwiper = new Swiper('#construction-slider .swiper-container', {
+        navigation: {
+            nextEl: '#construction-slider .swiper-button-next',
+            prevEl: '#construction-slider .swiper-button-prev',
+        },
+        pagination: {
+            el: '#construction-slider .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        }
+    });
+
+    $(".flat-card").each(function(index, element){
+        var $this = $(this);
+        $this.addClass("instance-" + index);
+        $this.find(".swiper-button-prev").addClass("btn-prev-" + index);
+        $this.find(".swiper-button-next").addClass("btn-next-" + index);
+        $this.find(".swiper-pagination").addClass("pagination-" + index);
+        var swiper = new Swiper(".instance-" + index + " .swiper-container", {
+            navigation: {
+                nextEl: ".btn-next-" + index,
+                prevEl: ".btn-prev-" + index,
+            },
+            pagination: {
+                el: ".pagination-" + index,
+                type: 'bullets',
+                clickable: true,
+            }
+        });
+    });
+
+    const newsSwiper = new Swiper('#news-slider .swiper-container', {
+        slidesPerView: 'auto',
+        navigation: {
+            nextEl: '#news-slider .swiper-button-next',
+            prevEl: '#news-slider .swiper-button-prev',
+        },
+        pagination: {
+            el: '#news-slider .swiper-pagination',
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+                return current + ' / ' + total;
+            }
+        }
     });
 });
 
